eac15f7d593e540b2b24e123c1fd75e7009516a9
mfehrenbach <mfehrenbach@users.noreply.github.com>
2022-07-11 20:37:55 +0000

    Discount JS “bundling” for Viewporter. 7014c6446288f330a2a85fb38a35a8fdfe3869fe

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,83 @@
-console.log('Hello, world.')
+const viewporter = () => {
+	let orientationChanged = false
+	let vhOffset = 0
+
+	const html = document.documentElement
+
+	const checkElementDims = (cssProperties) => {
+		const testElement = document.createElement('div')
+		testElement.style.cssText = cssProperties
+		html.insertBefore(testElement, html.firstChild)
+		const testElementDims = {
+			'width': testElement.offsetWidth,
+			'height': testElement.offsetHeight
+		}
+		html.removeChild(testElement)
+		return testElementDims
+	}
+
+	const updateValue = (cssVariable, cssValue) => {
+		cssValue ? html.style.setProperty(cssVariable, ` ${cssValue}px`) : html.style.removeProperty(cssVariable)
+	}
+
+	const updateVhOffset = () => {
+		const initialVisibleHeight = checkElementDims('position: fixed; top: 0; bottom: 0;')['height']
+		// Comparison for iOS in landscape, which does weird things with bottom-fixed elements.
+		const visibleHeight = window.innerHeight;
+		const vh100 = checkElementDims('position: fixed; top: 0; height: 100vh')['height']
+		vhOffset = vh100 - Math.min(initialVisibleHeight, visibleHeight)
+
+		updateValue('--vh--offset', vhOffset)
+		if (vhOffset) updateValue('--vh--initial', visibleHeight)
+	}
+
+	const updateInnerHeight = () => {
+		if (vhOffset) updateValue('--inner-height', window.innerHeight)
+	}
+
+	const updateScrollbarWidth = () => {
+		html.style.setProperty(`overflow-y`, ` scroll`)
+		const percent100 = checkElementDims('position: fixed; top: 0; width: 100%; height: 200vh;')['width']
+		const scrollbarWidth = window.innerWidth - percent100
+		html.style.removeProperty(`overflow-y`)
+		updateValue('--scrollbar', scrollbarWidth)
+	}
+
+	const orientationDirection = () => {
+		// Which way is an iPhone rotated?
+		if (navigator.userAgent.includes('AppleWebKit') && !navigator.userAgent.includes('Chrome')) {
+			(window.orientation == 90) ? html.classList.add('counter-clockwise'): html.classList.remove('counter-clockwise');
+			(window.orientation == -90) ? html.classList.add('clockwise'): html.classList.remove('clockwise');
+		}
+	}
+
+
+
+	document.addEventListener('DOMContentLoaded', () => {
+		if (navigator.appVersion.includes("Win")) html.classList.add('windows')
+
+		updateVhOffset()
+		updateInnerHeight()
+		updateScrollbarWidth()
+		orientationDirection()
+	})
+
+	window.addEventListener('orientationchange', () => {
+		orientationChanged = true
+
+		orientationDirection()
+		setTimeout(() => window.dispatchEvent(new Event('resize')), 250); // For other things listening that are a bit behind.
+		setTimeout(() => orientationChanged = false, 750)
+	})
+
+	window.addEventListener('resize', () => {
+		updateScrollbarWidth()
+		updateInnerHeight()
+
+		if (orientationChanged) updateVhOffset()
+	})
+}
+
+
+
+viewporter()

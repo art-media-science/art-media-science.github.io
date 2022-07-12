6a2a4ddcfb9c2928da34f5e2e17a3c8e816fa8da
Michael Fehrenbach <mfehrenbach@gmail.com>
2022-07-12 16:20:34 -0400

    Toss first attempt at scaling.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,18 +3,5 @@
 ---
 
 
+
 {% include_relative _viewporter.js %}
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-		const mainElement = document.querySelector('main')
-		const mainElementClass = 'main--visible'
-
-		const mainElementVisible = new IntersectionObserver(entries => {
-			const [entry] = entries
-			entry.isIntersecting ? document.body.classList.add(mainElementClass) : document.body.classList.remove(mainElementClass);
-		})
-
-		mainElementVisible.observe(mainElement)
-})

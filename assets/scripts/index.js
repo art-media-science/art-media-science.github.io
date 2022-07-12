64b756d15d7944df40fd88e969bbe0cd6f01dc58
mfehrenbach <mfehrenbach@users.noreply.github.com>
2022-07-12 21:49:48 +0000

    IntersectionObserver scaling, but much better. 4b310910abb5f35ea9c625b5020525a0e7279839

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -77,3 +77,48 @@
 		if (orientationChanged) updateVhOffset()
 	})
 })()
+
+
+
+
+const getHeaderDimensions = () => {
+	const body = document.body.style
+	const logo = document.getElementById('logo').firstElementChild
+	const tagline = document.getElementById('tagline')
+
+	const logoHeightVar = '--logo--height'
+	const taglineHeightVar = '--tagline--height'
+
+	let logoHeight = logo.offsetHeight
+	let taglineHeight = tagline.offsetHeight
+
+	body.setProperty(logoHeightVar, ` ${logoHeight / 10}rem`)
+	body.setProperty(taglineHeightVar, ` ${taglineHeight / 10}rem`)
+
+	window.addEventListener('resize', () => {
+		logoHeight = logo.offsetHeight
+		taglineHeight = tagline.offsetHeight
+
+		body.setProperty(logoHeightVar, ` ${logoHeight / 10}rem`)
+		body.setProperty(taglineHeightVar, ` ${taglineHeight / 10}rem`)
+	})
+}
+
+const scrollHeaderScale = () => {
+	const main = document.querySelector('main')
+	const mainClass = 'main--visible'
+
+	const mainVisible = new IntersectionObserver(entries => {
+		const [entry] = entries
+		entry.isIntersecting ? document.body.classList.add(mainClass) : document.body.classList.remove(mainClass);
+	})
+
+	mainVisible.observe(main)
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	getHeaderDimensions()
+	scrollHeaderScale()
+})

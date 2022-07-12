c0feeb00058809e91a570a3683ae405898d666a7
Michael Fehrenbach <mfehrenbach@gmail.com>
2022-07-11 23:26:39 -0400

    First bobbly try at scaling, with `IntersectionObserver`.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,3 +4,17 @@
 
 
 {% include_relative _viewporter.js %}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+		const mainElement = document.querySelector('main')
+		const mainElementClass = 'main--visible'
+
+		const mainElementVisible = new IntersectionObserver(entries => {
+			const [entry] = entries
+			entry.isIntersecting ? document.body.classList.add(mainElementClass) : document.body.classList.remove(mainElementClass);
+		})
+
+		mainElementVisible.observe(mainElement)
+})

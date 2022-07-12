99cf53562161b808f5ca751491f499a84a492313
Michael Fehrenbach <mfehrenbach@gmail.com>
2022-07-12 17:03:23 -0400

    Javascript-measured logo/tagline variables.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,34 @@
 
 
 {% include_relative _viewporter.js %}
+
+
+
+const getHeaderDimensions = () => {
+	const body = document.body.style
+	const logo = document.getElementById('logo').firstElementChild
+	const tagline = document.getElementById('tagline')
+
+	const logoHeightVar = '--logo--height'
+	const taglineHeightVar = '--tagline--height'
+
+	let logoHeight = logo.offsetHeight
+	let taglineHeight = tagline.offsetHeight
+
+	body.setProperty(logoHeightVar, ` ${logoHeight / 10}rem`)
+	body.setProperty(taglineHeightVar, ` ${taglineHeight / 10}rem`)
+
+	window.addEventListener('resize', () => {
+		logoHeight = logo.offsetHeight
+		taglineHeight = tagline.offsetHeight
+
+		body.setProperty(logoHeightVar, ` ${logoHeight / 10}rem`)
+		body.setProperty(taglineHeightVar, ` ${taglineHeight / 10}rem`)
+	})
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	getHeaderDimensions();
+})

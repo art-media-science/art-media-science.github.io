886d281d9ca4b770594b84eb9d82d9e392efec8d
mfehrenbach <mfehrenbach@users.noreply.github.com>
2022-07-12 03:28:56 +0000

    First bobbly try at scaling, with `IntersectionObserver`. c0feeb00058809e91a570a3683ae405898d666a7

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -77,3 +77,18 @@
 		if (orientationChanged) updateVhOffset()
 	})
 })()
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	const mainElement = document.querySelector('main')
+	const mainElementClass = 'main--visible'
+
+	const mainElementVisible = new IntersectionObserver(entries => {
+		const [entry] = entries
+		entry.isIntersecting ? document.body.classList.add(mainElementClass) : document.body.classList.remove(mainElementClass);
+	})
+
+	mainElementVisible.observe(mainElement)
+})

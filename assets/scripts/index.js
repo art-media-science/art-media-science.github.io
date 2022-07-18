b8d3a29324178d7c9b7e1a3e4e5e70ac7ff9258b
Michael Fehrenbach <mfehrenbach@gmail.com>
2022-07-18 13:41:41 -0400

    Fix for iPhone sticky flickering.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -107,6 +107,20 @@ const activeSection = (logo, activeClass) => {
 
 
 
+const fixIphoneFlicker = (...elements) => {
+	// Only on iPhones, of course.
+	if (navigator.platform.includes('iPhone')) {
+		elements.forEach((element) => {
+			window.addEventListener('scroll', () => {
+				// “Trill” the opacity to force a re-render while scrolling.
+				(element.style.opacity == 1) ? element.style.opacity = 0.999 : element.style.opacity = 1
+			})
+		})
+	}
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
 	const body =    document.body
 	const header =  document.querySelector('[data-header]')
@@ -119,4 +133,5 @@ document.addEventListener('DOMContentLoaded', () => {
 	logoScrollScale(body, logo)
 	invertBackground(body, nouns, links, invertClass)
 	activeSection(logo, activeClass)
+	fixIphoneFlicker(logo, tagline)
 })
